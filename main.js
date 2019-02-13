a64f4dfae4f741a44448d9467c743a0ac831b4c1
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-13 16:16:06 +0900

    条件分岐

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: '#app',
   data: {
     message: '初期メッセージ',
-    list: ['りんご', 'ばなな', 'いちご']
+    list: ['りんご', 'ばなな', 'いちご'],
+    show: true
   },
   methods: {
     handleClick: function(event) {
