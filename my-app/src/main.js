0b0769718a141a6a0ee3cdfcb9fdecf3aa533b99
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-21 23:56:24 +0900

    インポートの場所を変更

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,11 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'babel-polyfill'
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
-
-Vue.use(Vuex)
+import store from './store.js'
 
 Vue.config.productionTip = false
 
@@ -13,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   components: { App },
+  store,
   template: '<App/>'
 })
