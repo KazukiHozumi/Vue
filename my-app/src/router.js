d963e19acd9e3fd55b737134002beaa807abaea4
KazukiHozumi <hozumikazuki.8140@gmail.com>
2019-02-22 21:57:11 +0900

    パターンマッチングのルーティング

--- my-app/src/router.js
+++ my-app/src/router.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import ProductList from '@/views/ProductList'
 import Product from '@/views/Product.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: Home },
-    { path: '/product', component: Product }
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/product',
+      component: ProductList
+    },
+    {
+      path: '/product/:id(\\d+)',
+      component: Product
+    }
   ]
 })
 
---
+++ my-app/src/views/ProductList.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="procuct-list"><h1>商品一覧</h1></div>
+</template>
